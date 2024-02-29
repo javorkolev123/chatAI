08e35ea6ac02cbd888ae1caaeac416708da917e2
javorkolev123 <javorkolev123@gmail.com>
2024-02-29 16:31:30 -0600

    Refine file loading

--- loader/loader.py
+++ loader/loader.py
@@ -1,7 +1,10 @@
+import json
 import os.path
+import uuid
 
+import chromadb
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader, PyPDFLoader
 from langchain_core.documents.base import Document
 
 
@@ -10,18 +13,121 @@ def _add_directory(d: Document):
     d.metadata['dir'] = os.path.dirname(source)
 
 
-class DirectoryLoader:
+class Loader:
+    """
+    The Loader class takes a documents directory path relative
+    to the execution path and a DB collection client. The class parses
+    the directory and updates the DB collection entries as required.
+    """
 
-    def __init__(self, directory_path: str):
+    def __init__(self, directory_path: str, collection: chromadb.Collection):
         self._directory_path = directory_path
+        self._collection = collection
 
-    def load_directory(self) -> list[Document]:
-        loader = PyPDFDirectoryLoader(self._directory_path, recursive=True)
-        docs = loader.load()
+    def load(self):
+        """
+        Load the directory data into the DB collection.
+        :return:
+        """
+        self._load_directory(self._directory_path)
 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000, chunk_overlap=200, add_start_index=True
+    def _add_to_collection(self, documents: list[Document]):
+        if len(documents) < 1:
+            return
+        ids = [uuid.uuid4().hex for _ in documents]
+        metadatas = [i.metadata for i in documents]
+        texts = [i.page_content for i in documents]
+        [_add_directory(i) for i in documents]
+        self._collection.add(
+            ids=ids,
+            metadatas=metadatas,
+            documents=texts,
         )
-        all_splits = text_splitter.split_documents(docs)
-        [_add_directory(i) for i in all_splits]
-        return all_splits
+
+    def _delete_from_collection(self, document_names: list[str]):
+        if len(document_names) == 0:
+            return
+        elif len(document_names) == 1:
+            self._collection.delete(where={"source": document_names[0]})
+        else:
+            filters = []
+            [(filters.append({"source": i})) for i in document_names]
+            self._collection.delete(where={"$or": filters})
+
+    def _load_directory(self, path: str):
+        cur_files, dirs = {}, []
+        special = ""
+
+        for p in os.listdir(path):
+            full_path = os.path.join(path, p)
+            if os.path.isfile(full_path):
+                if p == ".files":
+                    special = full_path
+                    continue
+                # Hash the float the guarantee no weirdness from saving floats as text
+                cur_files[full_path] = os.path.getmtime(full_path).hex()
+                continue
+            dirs.append(full_path)
+
+        if special == "":
+            # if directory has not been visited before
+            # load all files and then dump the dictionary
+            special = os.path.join(path, ".files")
+            loader = PyPDFDirectoryLoader(path, recursive=False)
+            docs = loader.load()
+
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000, chunk_overlap=200, add_start_index=True
+            )
+            all_splits = text_splitter.split_documents(docs)
+            self._add_to_collection(all_splits)
+
+            with open(special, 'x') as f:
+                f.write(json.dumps(cur_files))
+        else:
+            with open(special, 'r') as f:
+                old_files = json.loads(f.read())
+
+            new_dot_file = {}
+            docs_to_add = []
+            docs_to_delete: list[str] = []
+
+            for k, v in old_files.items():
+                cur = cur_files.get(k)
+                if cur is None:
+                    # Deleted files
+                    print(f'{k} to be deleted')
+                    docs_to_delete.append(k)
+                    continue
+
+                if v != cur:
+                    # Updated files
+                    print(f'{k} to be updated')
+                    docs_to_delete.append(k)
+                    loader = PyPDFLoader(k)
+                    docs = loader.load_and_split()
+                    docs_to_add.extend(docs)
+
+                new_dot_file[k] = cur
+                # Leave only new files in cur_files
+                del cur_files[k]
+
+            for k, v in cur_files.items():
+                # Those are all the new files we need to add
+                print(f'{k} to be added')
+                new_dot_file[k] = v
+                loader = PyPDFLoader(k)
+                docs = loader.load_and_split()
+                docs_to_add.extend(docs)
+
+            # Delete deleted files and old versions of
+            # updated files. Then add the new versions
+            # of updated files and newly added files.
+            self._delete_from_collection(docs_to_delete)
+            self._add_to_collection(docs_to_add)
+
+            with open(special, 'w') as f:
+                f.write(json.dumps(new_dot_file))
+
+        for dir_path in dirs:
+            self._load_directory(dir_path)
--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import os.path
-import hashlib
 
 import dotenv
 import chromadb
-from chromadb.errors import IDAlreadyExistsError
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_community.vectorstores import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
-from loader.loader import DirectoryLoader
+from loader.loader import Loader
 import chromadb.utils.embedding_functions as embedding_functions
 
 OPENAI_KEY = 'OPENAI_API_KEY'
@@ -28,11 +24,11 @@ COLLECTION = "PDFChat"
 
 
 def main():
+    # TODO: Add option to use local models.
+    # TODO: Add option to choose OpenAI model.
     dotenv.load_dotenv()
-    documents = DirectoryLoader("Documents").load_directory()
-    print(documents[0].metadata)
 
-    # load if it already exists
+    # Create DB and load data
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
         api_key=os.environ[OPENAI_KEY],
         model_name="text-embedding-ada-002"
@@ -42,20 +38,7 @@ def main():
         name=COLLECTION,
         embedding_function=openai_ef,
     )
-
-    try:
-        ids = [hashlib.md5(i.page_content.encode()).hexdigest() for i in documents]
-        metadatas = [i.metadata for i in documents]
-        texts = [i.page_content for i in documents]
-        collection.add(
-            ids=ids,
-            metadatas=metadatas,
-            documents=texts,
-        )
-    except IDAlreadyExistsError as id_error:
-        # This means that some files were already loaded before.
-        # This is expected for now.
-        print(f"Files were already loaded => {id_error}")
+    Loader("Documents", collection).load()
 
     # PDF retriever
     vectorstore = Chroma(
@@ -64,10 +47,12 @@ def main():
         embedding_function=OpenAIEmbeddings(),
     )
 
+    # TODO: Add option to limit search to a certain directory
     retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
     prompt_template = PromptTemplate.from_template(TEMPLATE)
 
+    # TODO: Add dialogue memory
     rag_chain_from_docs = (
             RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
             | prompt_template
